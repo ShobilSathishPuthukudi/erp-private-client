f3f736840dbaea4a173e9f3c60f785d975e4b570
Shobil Sathish <shobilsathishputhukudi@gmail.com>
2026-04-10 15:12:43 +0530

    fix: import useMemo in OpsDashboard focus

--- client/src/pages/academic/OpsDashboard.tsx
+++ client/src/pages/academic/OpsDashboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import { NavLink } from 'react-router-dom';
 import { api } from '@/lib/api';
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
